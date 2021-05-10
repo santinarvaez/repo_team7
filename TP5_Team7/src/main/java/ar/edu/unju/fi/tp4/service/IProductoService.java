7dfb2d63f7ec40ed4b58b50a97bd48e29e283bbb
Santiago <Santiago@DucksPC>
2021-05-10 01:34:49 -0300

    Task: Creación de la nueva carpeta en el repositorio del tp5

---
+++ TP5_Team7/src/main/java/ar/edu/unju/fi/tp4/service/IProductoService.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.tp4.service;
+
+
+import java.util.List;
+
+import ar.edu.unju.fi.tp4.model.Producto;
+
+public interface IProductoService {
+
+	public void addProducto(Producto producto);
+	public Producto getUltimoProducto();
+	public Producto getProducto();
+	public List<Producto> getAllProductos();
+}
