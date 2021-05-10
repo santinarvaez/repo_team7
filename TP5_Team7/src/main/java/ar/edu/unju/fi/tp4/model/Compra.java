30e38a5a94b75010ebf7239b10ba5fa9b9c3661b
Santiago <Santiago@DucksPC>
2021-05-10 04:29:59 -0300

    Task: Agregando la clase de "Compra" al model

---
+++ TP5_Team7/src/main/java/ar/edu/unju/fi/tp4/model/Compra.java
@@ -0,0 +1,70 @@
+package ar.edu.unju.fi.tp4.model;
+
+public class Compra {
+		private int id;
+		private Producto producto;
+		private int cantidad;
+		private double total;
+		
+		
+		//CONSTRUCTORES
+		
+		public Compra() {
+			// TODO Auto-generated constructor stub
+		}
+		
+		
+		/**
+		 * 
+		 * @param id
+		 * @param producto
+		 * @param cantidad
+		 * @param total
+		 */
+		
+		public Compra(int id, Producto producto, int cantidad, double total) {
+			super();
+			this.id = id;
+			this.producto = producto;
+			this.cantidad = cantidad;
+			this.total = total;
+		}
+		
+		//GETTERS AND SETTERS
+		
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public Producto getProducto() {
+			return producto;
+		}
+
+		public void setProducto(Producto producto) {
+			this.producto = producto;
+		}
+
+		public int getCantidad() {
+			return cantidad;
+		}
+
+		public void setCantidad(int cantidad) {
+			this.cantidad = cantidad;
+		}
+
+		public double getTotal() {
+			return total;
+		}
+
+		public void setTotal(double total) {
+			this.total = total;
+		}
+
+		
+		
+		
+}
